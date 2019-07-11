b4eefcba4cd727a7a18d7f01c089e9572575ce90
winston augustus chung <18.chung.winston@ctkprep.org>
2019-07-11 22:51:01 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -37,11 +37,11 @@ def monopoly_with_third_tier
 	    	  :pennsylvania_railroad=>
 	    	  {
 	    	    
-	    	  }
+	    	  },
 	    	  :b_and_o_railroad=>
 	    	  {
 	    	    
-	    	  }
+	    	  },
 	    	  :shortline=>
 	    	  {
 	    	    
