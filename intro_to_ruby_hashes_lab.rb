783f1a81078c4b35280e1b5ac7a1d7f26066d9f0
winston augustus chung <18.chung.winston@ctkprep.org>
2019-07-11 22:48:26 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -30,7 +30,10 @@ def monopoly_with_third_tier
 	    	:pieces=>4,
 	    	:names=>
 	    	{
-	    	  
+	    	  :reading_railroad=>
+	    	  {
+	    	    
+	    	  }
 	    	},
 	    	:rent_in_dollars=>
 	    	{
